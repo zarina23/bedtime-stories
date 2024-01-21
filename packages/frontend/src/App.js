23da94e520c42eeea8ade07e43e0bafc40df2bc9
Zarina Urazgaleyeva <urazgaleyeva.zarina@gmail.com>
2024-01-21 18:22:30 +0100

    Initial frontend config.

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,7 @@
+const App = (props) => {
+    console.log(props);
+
+    return (<div>{'hello world'}</div>);
+};
+
+export default App;
---
+++ packages/frontend/webpack.config.js
@@ -0,0 +1,99 @@
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+module.exports = async () => {
+    const {
+        env: {
+            NODE_ENV
+        }
+    } = process;
+
+    const plugins = [
+        new Dotenv({
+            systemvars: true
+        }),
+        new HtmlWebpackPlugin({
+            // favicon: './src/assets/icons/favicon.ico',
+            filename: 'index.html',
+            path: path.join(__dirname, '../dist/'),
+            template: './src/index.html'
+        })
+    ];
+
+    const entry = './src/index.js';
+
+    return ({
+        devServer: {
+            historyApiFallback: true,
+            hot: true
+        },
+        entry,
+        mode: NODE_ENV,
+        module: {
+            rules: [
+                {
+                    exclude: /node_modules/,
+                    test: /\.jsx?$/i,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        targets: {
+                                            chrome: 100
+                                        }
+                                    }
+                                ],
+                                '@babel/preset-react'
+                            ],
+                            sourceType: 'unambiguous'
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    auto: /^.*(colors|units).scss$/
+                                }
+                            }
+                        },
+                        'postcss-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        output: {
+            clean: true,
+            filename: 'bundle.[fullhash].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins,
+        resolve: {
+            alias: {
+                '@components': path.resolve(__dirname, 'src/components'),
+                '@constants': path.resolve(__dirname, 'src/constants.ts'),
+                '@modules': path.resolve(__dirname, 'src/modules'),
+                '@styles': path.resolve(__dirname, 'src/styles'),
+                '@utils': path.resolve(__dirname, 'src/utils')
+            },
+            extensions: [
+                '.jsx',
+                '.js'
+            ]
+        }
+    });
+};
